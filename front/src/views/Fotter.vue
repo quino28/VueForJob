a3bd05357737a106ad7d7b7e5e7a3ea30ac926e7
Rin.M <quino@RM.lan>
2022-05-19 01:01:15 +0300

    move front directory

---
+++ front/src/views/Fotter.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="footer">
+    <p>made by quino</p>
+    <p>email: quinolactacin@gmail.com</p>
+  </div>
+</template>
+
+<style lang="scss">
+.footer {
+  background-color: #8b968d;
+  color: #fef4f4;
+  padding: 20px;
+  font-size: 14px;
+}
+</style>
---
+++ front/src/views/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+    <ul>
+      <li>News</li>
+      <li>Profile</li>
+      <li>Fun Club</li>
+      <li>Dog API</li>
+    </ul>
+  </div>
+</template>
+
+<style lang="scss">
+.header {
+  background-color: #38b48b;
+  color: #fef4f4;
+  padding: 20px;
+  li {
+    display: inline;
+    margin: 10px;
+    cursor: pointer;
+  }
+}
+</style>
