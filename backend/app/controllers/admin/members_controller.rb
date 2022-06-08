df9bb9f2e820ad74ae16e0084aa8334d40f433c0
quino <quinolactacin@gmail.com>
2022-06-08 17:16:54 +0300

    add admin_controller

--- backend/app/controllers/admin/members_controller.rb
+++ backend/app/controllers/admin/members_controller.rb
@@ -1,15 +1,8 @@
-class Admin::MembersController < ApplicationController
-  require 'logger'
-  log = Logger.new('./rubyflie.log')
-
+class Admin::MembersController < AdminController
   def index
-    log.debug('test')
   end
 
   def show
-    require 'logger'
-    log = Logger.new('./rubyflie.log')
-    log.debug('show')
-    render "members/show"
+    render 'members/show'
   end
 end
