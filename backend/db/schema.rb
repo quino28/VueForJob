3f9147c366c17763472369c27cdcfff6ce7dfa5a
quino <quinolactacin@gmail.com>
2022-07-14 17:08:00 +0300

    add members table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,11 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_13_132433) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_14_140043) do
   create_table "admin_members", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "members", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "address"
+    t.string "email", null: false
+    t.date "birthday"
+    t.string "gender", null: false
+    t.string "password_digest", null: false
+    t.integer "payment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
