277ea9703a657e52957491a11f65897f6e2b6d6d
quino <quinolactacin@gmail.com>
2022-07-24 10:25:20 +0300

    add member new page

--- backend/app/controllers/admin/members_controller.rb
+++ backend/app/controllers/admin/members_controller.rb
@@ -1,3 +1,15 @@
 class Admin::MembersController < AdminController
   before_action :logged_in_admin_member
+
+  private
+  def get_params
+    params.permit(
+      :name,
+      :email,
+      :birthday,
+      :gender,
+      :password,
+      :password_confirmation
+    )
+  end
 end
--- backend/app/models/members.rb
+++ backend/app/models/members.rb
@@ -1,2 +1,6 @@
 class Members < ApplicationRecord
+  has_secure_password
+
+  validates :name, :email, :birthday, :gender, presence: true
+  validates :email, uniqueness: { message: 'Duplicated.' }
 end
