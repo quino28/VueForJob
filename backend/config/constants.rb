847f6a4f340c35504288ea87c8e425685e5f9750
quino <quinolactacin@gmail.com>
2022-07-27 15:17:35 +0300

    add constants file

---
+++ backend/config/constants.rb
@@ -0,0 +1,8 @@
+module Constants
+  GENDER = {
+    1: 'Male',
+    2: 'Female',
+    3: 'Otherwise',
+  }
+end
+Constants.freeze
--- backend/config/environment.rb
+++ backend/config/environment.rb
@@ -1,5 +1,8 @@
 # Load the Rails application.
 require_relative "application"
 
+# Load the Rails constants.
+require_relative "constants"
+
 # Initialize the Rails application.
 Rails.application.initialize!
