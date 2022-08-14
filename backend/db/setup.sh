04c821f0661aaa8ee34b552ccf0fce5d6ae627ff
quino <quinolactacin@gmail.com>
2022-08-14 18:02:17 +0300

    set init method in setup.sh

--- backend/db/setup.sh
+++ backend/db/setup.sh
@@ -4,3 +4,23 @@ set -eu
 
 RAILS_ROOT=$(cd $(dirname $0)/../ && pwd)
 DOCKER_COMPOSE_FILE="${RAILS_ROOT}/docker-compose.yml"
+
+function usage() {
+  cat <<EOF
+Usage:
+  bash $0 [command]
+Commands:
+  init initialize
+EOF
+}
+
+function init() {
+    echo 'test'
+}
+
+if [ $# -eq 0 ];then
+  usage
+  exit 1
+fi
+
+$1
