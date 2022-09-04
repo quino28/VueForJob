3240ef3ee1a45360e84f46d1062de788a1dd7d02
RinM <quinolactacin@gmail.com>
2022-09-04 20:22:42 +0300

    add form in login page

--- front/src/components/Body/Login.vue
+++ front/src/components/Body/Login.vue
@@ -1,3 +1,36 @@
 <template>
   <h1>Login page</h1>
+  <div class="container">
+    <div class="mb-3 row">
+      <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+      <div class="col-sm-10">
+        <input type="email" class="form-control" id="email" placeholder="Email">
+      </div>
+    </div>
+    <div class="mb-3 row">
+      <label for="password" class="col-sm-2 col-form-label">Password</label>
+      <div class="col-sm-10">
+        <input type="password" class="form-control" id="password" placeholder="password">
+      </div>
+    </div>
+    <div class="mb-3 row">
+      <label for="password" class="col-sm-2 col-form-label"></label>
+      <div class="col-sm-10">
+        <button type="submit" class="btn btn-primary">Login</button>
+      </div>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        email: '',
+        password: '',
+      },
+    }
+  },
+}
+</script>
