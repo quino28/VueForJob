55f7e2905c5dae5f270455412697abb18c775366
RinM <quinolactacin@gmail.com>
2022-11-10 16:03:10 +0100

    install vue-router

--- front/src/main.js
+++ front/src/main.js
@@ -5,11 +5,14 @@ import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import './assets/css/reset.css'
+import Router from './router.js'
 import Store from './store.js'
 
-const app = createApp(App).use(Store)
+const app = createApp(App)
 
-app.use(BootstrapVue3)
 app.use(Axios)
+app.use(BootstrapVue3)
+app.use(Router)
+app.use(Store)
 
 app.mount('#app')
