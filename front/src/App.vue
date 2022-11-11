7c41223ad38472994d0790170facddb8d11469e8
RinM <quinolactacin@gmail.com>
2022-11-11 15:35:17 +0100

    add router.js

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,23 +1,31 @@
 <template>
-  <Header
-    @changeComponent="changeComponent"
-  />
+  <div class="header">
+    <ul>
+      <li>News</li>
+      <li>Profile</li>
+      <li>Fun Club</li>
+      <li @click="changeComponent('DogApi')">Dog API</li>
+    </ul>
+    <b-button variant="outline-light" @click="changeComponent('login')" v-if="!$store.state.member">Login</b-button>
+    <b-button variant="outline-light" @click="logout" v-else>Logout</b-button>
+  </div>
   <Body
     :currentComponent="this.currentComponent"
   />
-  <Fotter />
+  <div class="footer">
+    <p>made by quino</p>
+    <p>
+      email: <a href="mailto:quinolactacin@gmail.com">quinolactacin@gmail.com</a>
+    </p>
+  </div>
 </template>
 
 <script>
-import Header from './views/Header.vue'
 import Body from './views/Body.vue'
-import Fotter from './views/Fotter.vue'
 
 export default {
   components: {
-    Header,
     Body,
-    Fotter,
   },
   data() {
     return {
@@ -27,17 +35,49 @@ export default {
   methods: {
     changeComponent(component) {
       this.currentComponent = component
-    }
+    },
+    logout() {
+      this.$store.commit('removeMember')
+      this.currentComponent = 'home'
+    },
   }
 }
 </script>
 
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.header {
+  background-color: #38b48b;
+  color: #fef4f4;
+  padding: 20px;
+  display: flex;
+  align-items: center;
+  ul {
+    li {
+      display: inline;
+      margin: 10px;
+      cursor: pointer;
+    }
+  }
+  button {
+    margin-left: auto;
+  }
+}
+
+.footer {
+  background-color: #8b968d;
+  color: #fef4f4;
+  padding: 20px;
+  font-size: 14px;
+  a {
+    color: inherit;
+  }
 }
 </style>
